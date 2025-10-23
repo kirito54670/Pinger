ff493b8b36ed23ce82345007059e2fee36b25c55
kirito54670 <valolean@gmail.com>
2025-10-23 20:35:44 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+export const PING_URLS = [
+  'https://api1.onrender.com/',
+  'https://api2.onrender.com/',
+  'https://api3.onrender.com/'
+];
+
+// Rastgele dakika aralığı (min ve max) -> 1 ile 5 dakika
+export const MIN_MINUTES = 1;
+export const MAX_MINUTES = 5;
+
+// User-Agent (opsiyonel) -> Render/Glitch gibi servislerde bot gibi görünmeyi azaltır
+export const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
+                          '(KHTML, like Gecko) Chrome/118.0.5993.117 Safari/537.36';
